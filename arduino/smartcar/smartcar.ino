f88f96e1bff1aa150b97b6b55740e2f4574f6905
oodam <81178803+oodam@users.noreply.github.com>
2021-04-06 14:35:19 +0200

    Basic obstacle avoidance
    @Novenius 
    User story#2

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,9 +1,74 @@
 #include <Smartcar.h>
 
-void setup() {
-  // put your setup code here, to run once:
+ArduinoRuntime arduinoRuntime;
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+
+// const int frontPin = 0;
+// GP2Y0A02 sideFrontIR(arduinoRuntime,
+//                     frontPin); 
+
+const int backPin = 3;
+GP2Y0A02 sideBackIR(arduinoRuntime,
+                    backPin); 
+
+const int TRIGGER_PIN           = 6; // D6
+const int ECHO_PIN              = 7; // D7
+const unsigned int MAX_DISTANCE = 2000;
+SR04 front(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+
+SimpleCar car(control);
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.setTimeout(200);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    handleInput();
+    autoStop();
+    delay(100);
+   
 }
+
+void handleInput()
+{
+    // handle serial input if there is any
+    if (Serial.available())
+    {
+        String input = Serial.readStringUntil('\n');
+        if (input.startsWith("m"))
+        {
+            int throttle = input.substring(1).toInt(); 
+            car.setSpeed(throttle);
+        }
+        else if (input.startsWith("t"))
+        {
+            int deg = input.substring(1).toInt();
+            car.setAngle(deg);
+        }
+        
+        
+    }
+   
+}
+
+void autoStop(){
+  
+  if(front.getDistance() < 1000 && front.getDistance() > 0){ 
+                car.setSpeed(-10);
+                delay(1000);
+                car.setSpeed(0);
+                
+        }
+        Serial.println(front.getDistance());
+  
+  if(sideBackIR.getDistance() < 120 && sideBackIR.getDistance() > 0){ 
+                car.setSpeed(10);
+                
+        }
+  }
