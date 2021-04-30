f6df8e8bb57fe0f213d4f8095509d1045c9295f9
felix <felix@felix.fffff>
2021-04-30 11:37:47 +0200

    fixed smartcar sketch

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,6 +1,11 @@
 #include <MQTT.h>
 #include <WiFi.h>
 #include <Smartcar.h>
+#include <vector> 
+
+#ifdef __SMCE__
+#include <OV767X.h>
+#endif
 
 #ifndef __SMCE__
 WiFiClient net;
@@ -14,6 +19,7 @@ DifferentialControl control(leftMotor, rightMotor);
 
 SimpleCar car(control);
 
+std::vector<char> frameBuffer; 
 
 const int backPin = 3;
 GP2Y0A02 sideBackIR(arduinoRuntime,
@@ -24,13 +30,18 @@ const auto echoPin = 7;
 const auto maxDistance = 2000;
 SR04 front(arduinoRuntime, triggerPin, echoPin, maxDistance);
 
-bool x = true;
+bool canDriveForward = true;
+bool canDriveBackwards = true;
 
 void setup() {
-  
   Serial.begin(9600);
+
+ 
+
 #ifdef __SMCE__
-  
+
+  Camera.begin(QVGA, RGB888, 0); //qvga is a format 320 X 240, QVGA, RGB888, 0
+  frameBuffer.resize(Camera.width() * Camera.height() * Camera.bytesPerPixel());
   mqtt.begin("127.0.0.1", 1883, WiFi);
   // mqtt.begin(WiFi); // Will connect to localhost
 #else
@@ -40,7 +51,15 @@ void setup() {
     mqtt.subscribe("/smartcar/control/#", 1);
     mqtt.onMessage([](String topic, String message) {
       if (topic == "/smartcar/control/throttle") {
-        car.setSpeed(message.toInt());
+        if (canDriveForward && message.toInt() >= 0){
+          car.setSpeed(message.toInt());
+          } 
+          
+        if (canDriveBackwards && message.toInt() <= 0){
+            car.setSpeed(message.toInt());
+            }
+        
+            
       } else if (topic == "/smartcar/control/steering") {
         car.setAngle(message.toInt());
       } else {
@@ -53,10 +72,20 @@ void setup() {
 }
 
 void loop() {
-  car.setSpeed(0);
+  
   if (mqtt.connected()) { //the car is stupid and drives when starting
     mqtt.loop();
     // const auto currentTime = millis();
+    #ifdef __SMCE__
+    const auto currentTime = millis();
+    static auto previousFrame = 0UL;
+    if (currentTime - previousFrame >= 65) {
+      previousFrame = currentTime;
+      Camera.readFrame(frameBuffer.data());
+      mqtt.publish("/smartcar/camera", frameBuffer.data(), frameBuffer.size(),
+                   false, 0);
+    }
+    #endif
   }
   autoStop();
   delay(35);
@@ -64,19 +93,30 @@ void loop() {
 }
 
 void autoStop(){
+  int distance = front.getDistance();
+  if(distance < 120 && distance > 0){ 
+           if(canDriveForward){
+                car.setSpeed(0);   
+                canDriveForward = false;
+                Serial.println("cannot drive frontward");
+           }       
+        }else{
+           canDriveForward = true;
+           Serial.println("can drive frontward");
+          }
+        Serial.println(front.getDistance());
   
-  if(front.getDistance() < 120 && front.getDistance() > 0){ 
-    car.setSpeed(-50);
-    delay(3000);
-    car.setSpeed(0);
+  int backDistance = sideBackIR.getDistance();
+  if(backDistance < 120 && backDistance > 0){ 
+              if(canDriveBackwards){
+                car.setSpeed(0);   
+                canDriveBackwards = false;
+                Serial.println("cannot drive backwards");
+           }       
+        }else{
+           canDriveBackwards = true;
+           Serial.println("can drive backwards");
+          }
                 
-  }
-  Serial.println(front.getDistance());
-  
-  if(sideBackIR.getDistance() < 120 && sideBackIR.getDistance() > 0){ 
-    car.setSpeed(50);
-    delay(3000);
-    car.setSpeed(0);
-                
-  }
-}
+        }
+        
